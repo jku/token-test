a6e088a4279ed68c6835326096ca9860d3b5927d
Jussi Kukkonen <jkukkonen@google.com>
2023-03-10 15:24:48 +0200

    test signer

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+from sigstore.oidc import detect_credential
+from securesystemslib.signer import (
+    KEY_FOR_TYPE_AND_SCHEME,
+    Key,
+    SigstoreKey,
+    SigstoreSigner,
+)
+
+KEY_FOR_TYPE_AND_SCHEME[("sigstore-oidc", "Fulcio")] = SigstoreKey
+
+token = detect_credential()
+print(token)
+
+url = os.environ["GITHUB_SERVER_URL"]
+repo = os.environ["GITHUB_REPOSITORY"]
+ref = os.environ["GITHUB_REF"]
+
+print(f"{url}/{repo}/.github/workflows/test-sign.yml@{ref}"
