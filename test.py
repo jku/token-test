9e7c4c4e62f4430e6005ffe1790c231628aadd7d
Jussi Kukkonen <jkukkonen@google.com>
2023-03-10 15:56:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,6 @@ from securesystemslib.signer import (
 KEY_FOR_TYPE_AND_SCHEME[("sigstore-oidc", "Fulcio")] = SigstoreKey
 
 token = detect_credential()
-print(token)
-
 url = os.environ["GITHUB_SERVER_URL"]
 repo = os.environ["GITHUB_REPOSITORY"]
 ref = os.environ["GITHUB_REF"]
