ddb8c640a2d2280c4e0b322865fe1e3af67a3a93
Jussi Kukkonen <jkukkonen@google.com>
2023-03-10 15:40:15 +0200

    test signing

--- test.py
+++ test.py
@@ -16,4 +16,23 @@ url = os.environ["GITHUB_SERVER_URL"]
 repo = os.environ["GITHUB_REPOSITORY"]
 ref = os.environ["GITHUB_REF"]
 
-print(f"{url}/{repo}/.github/workflows/test-sign.yml@{ref}")
+identity = f"{url}/{repo}/.github/workflows/test-sign.yml@{ref}"
+issuer="https://token.actions.githubusercontent.com"
+
+# TODO this should happen in SigstoreSigner.import_()
+public_key = Key.from_dict(
+  "abcdef",
+  {
+    "keytype": "sigstore-oidc",
+    "scheme": "Fulcio",
+    "keyval": {
+      "issuer": issuer,
+      "identity": identity,
+    },
+  },
+)
+signer = SigstoreSigner(token, public_key)
+sig = signer.sign(b"data")
+
+print("KEY", public_key.to_dict())
+print("SIG", sig.to_dict())
